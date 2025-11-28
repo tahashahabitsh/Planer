593caed373ad0a33ea361f702a1fe74223801cb9
tahashahabitsh <taha.shahabitsh@gmail.com>
2025-11-28 02:53:42 +0000

    fix: import KeyboardOptions in AppLock screen

--- app/src/main/java/com/taha/planer/security/AppLock.kt
+++ app/src/main/java/com/taha/planer/security/AppLock.kt
@@ -17,6 +17,7 @@ import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.text.input.PasswordVisualTransformation
 import androidx.compose.ui.unit.dp
