914bb7b73213d50bd3e93e81564ba448a2625907
tahashahabitsh <taha.shahabitsh@gmail.com>
2025-11-28 08:34:05 +0000

    Fix DesignStyle reference in UiStyleHelpers

--- app/src/main/java/com/taha/planer/ui/UiStyleHelpers.kt
+++ app/src/main/java/com/taha/planer/ui/UiStyleHelpers.kt
@@ -1,4 +1,5 @@
-package com.taha.planer.ui
+package 
+import com.taha.planer.ui.DesignStylecom.taha.planer.ui
 import com.taha.planer.ui.DesignStyle
 
 import androidx.compose.material3.CardDefaults
