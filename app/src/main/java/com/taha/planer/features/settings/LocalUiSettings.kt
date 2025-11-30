6a229e72626aa5de34f73c19fb912b543a8ff31f
tahashahabitsh <taha.shahabitsh@gmail.com>
2025-11-30 06:40:57 +0000

    Fix LocalUiSettings redeclaration

--- app/src/main/java/com/taha/planer/features/settings/LocalUiSettings.kt
+++
@@ -1,9 +0,0 @@
-package com.taha.planer.features.settings
-
-import com.taha.planer.ui.DesignStyle
-import com.taha.planer.ui.ColorMode
-
-data class LocalUiSettings(
-    val designStyle: DesignStyle = DesignStyle.SYSTEM,
-    val colorMode: ColorMode = ColorMode.SYSTEM,
-)
